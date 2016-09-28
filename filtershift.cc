78393d7a590eba183287fed8e6091469e0855ca9
dpark6060 <dpark6060@gmail.com>
2016-09-28 16:46:20 -0400

    V1.2 Formatted "Usage" Output
    Formatted the "Usage" Output to fit on a standard 80 column terminal window.
    
    Also Changed the options to accept "-i " for input, and changed interleave to "--itl"

--- filtershift.cc
+++ filtershift.cc
@@ -23,67 +23,95 @@ using namespace NEWMAT;
 using namespace NEWIMAGE;
 using namespace Utilities;
 
-string title="FilterShift \nQNL's tool for optimal slice timing correction";
+string title="\n\nFilterShift \nQNL's tool for optimal slice timing correction";
 string examples="filtershift --in <InputFile> --tr <TR> [options]";
 
-Option<string> input(string("--in"), string(""),
-			  string("\tfilename the input image to perform STC on\n"),
+Option<string> input(string("-i,--in"), string(""),string(
+" filename the input image to perform STC on\n"),
 			  true, requires_argument);
 
-Option<float> TR(string("--TR"), 2.0,
-			  string("\tSet the TR of the original fMRI data in seconds\n"),
+Option<float> TR(string("--TR"), 2.0,string(
+" Set the TR of the original fMRI data in seconds\n"),
 			  true, requires_argument);
 
-Option<int> interleave(string("-i,--interleave"), 0, 
-			 string("\tset the interleave parameter, or how many slices are incramented between acquisitions\
-					\n\t\t\t\t\t1 = sequential acquisition\
-					\n\t\t\t\t\t2 = even/odd acquisition (acquire every second slice)\
-					\n\t\t\t\t\t3 = acquire every third slice, etc\n"), 
+Option<int> interleave(string("--itl"), 0, string(
+" set the interleave parameter, or how many slices are\
+\n\t\t\t incramented between acquisitions\
+\n\t\t\t 1 = sequential acquisition\
+\n\t\t\t 2 = even/odd acquisition (acquire every second slice)\
+\n\t\t\t 3 = acquire every third slice, etc\
+\n\t\t\t Leaving this blank will assume bottom up sequential\
+\n\t\t\t acquisition"), 
+			 true, requires_argument);
+
+Option<string> out(string("-o,--out"), string(""),string(
+	   " Specify an output file name - all working directories\
+\n\t\t\t will be created in the parent directory specified\
+\n\t\t\t here. Leave blank to run in the parent directory\
+\n\t\t\t of <InputFile>\n"), 
 			 false, requires_argument);
 
-Option<string> out(string("-o,--out"), string(""), 
-			 string("\tSpecify an output file name - all working directories will be created in the parent directory specified here.\
-					\n\t\t\t\t\tLeave blank to run in the parent directory of <InputFile>\n"), 
-			 false, requires_argument);
-
-Option<int> start(string("-s,--start"), 0,
-			  string("\tSet the starting slice - The slice that was acquired first in the sequence.  Default is slice 1, the bottom most slice.\
-					 \n\t\t\t\t\tThis starts the interleave from that slice.  If your interleave parameter is '1'\
-					 \n\t\t\t\t\tand your starting slice is '3', your slice acquisition sequence will be modled as:\
-					 \n\t\t\t\t\t3\n\t\t\t\t\t5\n\t\t\t\t\t7\n\t\t\t\t\t9...\n"),
+Option<int> start(string("-s,--start"), 0,string(
+	   " Set the starting slice - The slice that was acquired\
+\n\t\t\t first in the sequence. Default is slice 1, the bottom\
+\n\t\t\t most slice. This starts the interleave from that slice.\
+\n\t\t\t If your interleave parameter is '1' and your starting\
+\n\t\t\t slice is '3', your slice acquisition sequence will be\
+\n\t\t\t modled as:\
+\n\t\t\t\t 3\
+\n\t\t\t\t 5\
+\n\t\t\t\t 7\
+\n\t\t\t\t 9...\n"),
 			  false, requires_argument);
 			  
-Option<int> direction(string("-d,--direction"), 1, 
-			 string("\tvalue 1 or -1.  Set the direction of slice acquisition.\
-					\n\t\t\t\t\t 1: implies ascending slice acquisition from starting slice: (3,5,7,9...)\
-					\n\t\t\t\t\t-1: implies descending slice acquisition from starting slice: (7,5,3,...)\n"), 
+Option<int> direction(string("-d,--direction"), 1,string(
+		  " value 1 or -1.  Set the direction of slice \
+\n\t\t\t acquisition.\
+\n\t\t\t 1: ascending slice acquisition:(1,3,5,7,9...)\
+\n\t\t\t-1: descending slice acquisition: (9,7,5,3,...)\n"), 
 			 false, requires_argument);
 
-Option<string> order(string("--order"), string(""),
-			 string("\t\tSlice Order File.  This file is the order in which each slice was acquired. each row represents the order in which that slice was acquired.\
-					\n\t\t\t\t\t For example, '1' in the first row means that slice 1 was aquired first. '20' in the second row means that slice 2 was acquired 20th.\
-					\n\t\t\t\t\t If present, all interelave parameters are ignored, and slices are shifted using the slice order file\
-					\n\t\t\t\t\t we refer to the bottom slice in the image as slice 1, not slice 0\n"),
+Option<string> order(string("--order"), string(""),string(
+     "\t Slice Order File.  This file is the order in which\
+\n\t\t\t each slice was acquired. each row represents the\
+\n\t\t\t order in which that slice was acquired. For example,\
+\n\t\t\t '1' in the first row means that slice 1 was aquired\
+\n\t\t\t first. '20' in the second row means that slice 20 was\
+\n\t\t\t acquired 2nd. If present, all interelave parameters\
+\n\t\t\t are ignored, and slices are shifted using the slice\
+\n\t\t\t order file. we refer to the bottom slice in the image\
+\n\t\t\t as slice 1, not slice 0\n"),
 			 false, requires_argument);
 
-Option<float> lpf(string("--lpf"),0,
-				  string("\tSet the cutoff frequency of the lowpass filter in Hz.  \n\t\t\t\t\tNote that by default, this is set to include 80% of the total availible bandwidth.\n"),
+Option<float> lpf(string("--lpf"),0,string(
+	 "\t Set the cutoff frequency of the lowpass filter in Hz.\
+\n\t\t\t Note that by default, this is set to include 80% of\
+\n\t\t\t the total availible bandwidth.\n"),
 				  false,requires_argument);
 
-Option<string> timing(string("--timing"), string(""),
-			 string("\tSlice Timing File.  This file is the time at which each slice was acquired relative to the first slice. each row represents the time at which that slice was acquired.\
-\n\t\t\t\t\t For example, '0' in the first row means that slice 1 was aquired first, and will be shifted 0 seconds.  '0.5' in the second row\
-					  \n\t\t\t\t\t means that slice 2 was acquired 0.5 seconds after the first slice, and will be shifted 0.5s.\
-					  \n\t\t\t\t\t If present, all interelave parameters are ignored, and slices are shifted using the slice timing file.\
-					  \n\t\t\t\t\t If you put both a slice timing and a slice order, the program will yell at you and refuse to run.\n"),
+Option<string> timing(string("--timing"), string(""),string(
+	   " Slice Timing File.  This file is the time at which\
+\n\t\t\t each slice\ was acquired relative to the first slice.\
+\n\t\t\t each row represents the time at which that slice was\
+\n\t\t\t acquired. For example, '0' in the first row means\
+\n\t\t\t that slice 1 was aquired first, and will be shifted 0\
+\n\t\t\t seconds. '0.5' in the second row means that slice 2\
+\n\t\t\t was acquired 0.5 seconds after the first slice, and\
+\n\t\t\t will be shifted 0.5s. If present, all interelave\
+\n\t\t\t parameters are ignored, and slices are shifted using\
+\n\t\t\t the slice timing file. If you put both a slice timing\
+\n\t\t\t and a slice order, the program will yell at you and\
+\n\t\t\t refuse to run.\n"),
 			 false, requires_argument);
 
-Option<int> ref(string("-r,--reference"), 1,
-			 string("\tSet the Reference slice\n\t\t\t\t\tThis is the slice the data is aligned to.  Default is the first slice\n"),
+Option<int> ref(string("-r,--reference"), 1,string(
+	   " Set the Reference slice\
+\n\t\t\t This is the slice the data is aligned to.\
+\n\t\t\t Default is the first slice\n"),
 			 false, requires_argument);
 			      
   Option<bool> help(string("-h,--help"), false,
-		  string("\tdisplay this message\n"),
+		  string(" display this message\n"),
 		  false, no_argument);
 
 
